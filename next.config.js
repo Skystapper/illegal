c7a36e599c249b234e86f7ab0d31a424c8525a95
Skystapper <skystapper@gmail.com>
2024-12-31 16:58:15 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/illegal',
+  output: 'export',
   images: {
     domains: ['images.pexels.com'],
+    unoptimized: true,
   },
 }
 
